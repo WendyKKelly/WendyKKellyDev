6b46f1e48bc911b21a93f72c564ea1dc0052334c
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-01-22 20:37:24 -0800

    file path

--- components/data/index.js
+++ components/data/index.js
@@ -1,9 +1,9 @@
-import PortfolioImage1 from '../../image/portfolio-1.jpg';
-import PortfolioImage2 from '../../image/portfolio-2.jpg';
-import PortfolioImage3 from '../../image/portfolio-3.jpg';
-import PortfolioImage4 from '../../image/portfolio-4.jpg';
-import PortfolioImage5 from '../../image/portfolio-5.jpg';
-import PortfolioImage6 from '../../image/portfolio-6.jpg';
+import PortfolioImage1 from '../image/portfolio-1.jpg';
+import PortfolioImage2 from '../image/portfolio-2.jpg';
+import PortfolioImage3 from '../image/portfolio-3.jpg';
+import PortfolioImage4 from '../image/portfolio-4.jpg';
+import PortfolioImage5 from '../image/portfolio-5.jpg';
+import PortfolioImage6 from '../image/portfolio-6.jpg';
 
 export const PORTFOLIO_SHOWCASE = [
   {
